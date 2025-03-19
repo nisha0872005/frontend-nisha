f2deeb39aa1ce13c04da13de15cff9dba280f662
nisha <parvathikumar19038@gmail.com>
2025-03-19 17:40:24 +0530

    react changes

---
+++ New folder/saran/main.py
@@ -0,0 +1,4 @@
+string = "hello python"
+
+for char in string:
+    print(char)
--- react/sports-blog/src/App.js
+++ react/sports-blog/src/App.js
@@ -1,29 +1,31 @@
-import logo from './logo.svg';
+import tervon from './asset/img/tervon.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div style={{ fontFamily: "Work Sans, sans-serif", padding: "20px" }}>
-      <h1 style={{ fontFamily: "Montserrat, sans-serif", fontWeight: 800, color: "yellow", fontSize: "32px" }}>
+    <div className='container'>
+      <h1>
         Trevon Diggs makes it clear that his brother, Stefon, should leave Buffalo!
       </h1>
-      <img src="assets/img/tervon.jpg" alt="Trevon Diggs" style={{ maxWidth: "100%", height: "auto" }} />
+      <img src={tervon} alt="Trevon Diggs"  ></img>
 
       <h4>
         The injured Dallas Cowboys defensive back made it clear watching Monday Night Football last night: his brother, Stefon, needs to get out of Buffalo ASAP!
       </h4>
+      <img src={lasvegasnews} alt="Trevon Diggs" />
 
       <p>
         There is some MAJOR drama in Buffalo! The Bills offense is a shadow of itself in 2023 from external and internal stressors. 
         Last night, QB Josh Allen and the hometown Bills struggled in a third loss in four weeks. 
         This time to the often self-destructive Denver Broncos.
       </p>
+      <img src={lasvegas} alt="Trevon Diggs"   />
 
       <h3>Stefon Diggs, especially, has been frustrated.</h3>
       <p>
         The elite wideout has been seen vocalizing his frustrations with the coaching staff and his QB on the sideline during games over the last season and a half.
       </p>
-
+      <img src={lasvegaswin} alt="Trevon Diggs"  />
       <h2>Is this Stefon's last year with the Bills?</h2>
       <p>
         Rumblings on social media suggest that we could see a split between Stefon Diggs and the Buffalo Bills similar to the one we saw with the Minnesota Vikings a few years ago.
