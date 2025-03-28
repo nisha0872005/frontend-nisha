710df9fb5565c78d82fc5367b533a4794258162e
nisha <parvathikumar19038@gmail.com>
2025-03-28 17:29:22 +0530

    project changes

---
+++ react/viteproject/src/App.tsx
@@ -0,0 +1,15 @@
+
+import Cards from './components/cards/Cards'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Nisha</h1>
+      <Cards/>
+</>
+  )
+}
+
+export default App
